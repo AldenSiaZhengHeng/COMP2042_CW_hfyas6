6eaf6ad67bdad7c3f32f91a0e1ee1e06c1eda66e
AldenSiaZhengHeng <aldensia85@gmail.com>
2021-12-01 21:15:20 +0800

    Create a new class named scoreModel to store the data needed for scoring function

---
+++ src/score/scoreModel.java
@@ -0,0 +1,18 @@
+package score;
+
+public class scoreModel {
+    private int score;
+
+    public scoreModel(){
+
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int Score){
+        this.score = Score;
+    }
+
+}
