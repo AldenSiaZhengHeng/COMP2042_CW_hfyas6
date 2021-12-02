f1a59db657b175a16f040e616e55d6b82c91b8af
AldenSiaZhengHeng <aldensia85@gmail.com>
2021-12-02 21:42:46 +0800

    Create a new type of brick name diamond and assign the score for it

---
+++ src/element/DiamondBrick.java
@@ -0,0 +1,39 @@
+package element;
+
+import java.awt.*;
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
+public class DiamondBrick extends Brick {
+
+    private static final String NAME = "Diamond Brick";
+    private static final Color DEF_INNER = new Color(105, 255, 233);
+    private static final Color DEF_BORDER = Color.GRAY;
+    private static final int DIAMOND_STRENGTH = 5;
+
+    private static final int gainScore = 50;
+    private Shape brickFace;
+
+    public DiamondBrick(Point point, Dimension size) {
+        super(NAME, point, size,DEF_BORDER,DEF_INNER,DIAMOND_STRENGTH);
+        brickFace = super.getBrickFace();
+    }
+
+    public boolean setImpact(Point2D point , int dir){
+        if(super.isBroken()) {
+            return false;
+        }
+        super.impact(gainScore);
+        return super.isBroken();
+    }
+
+    @Override
+    public Shape getBrick() {
+        return brickFace;
+    }
+
+    @Override
+    protected Shape makeBrickFace(Point pos, Dimension size) {
+        return new Rectangle(pos, size);
+    }
+}
