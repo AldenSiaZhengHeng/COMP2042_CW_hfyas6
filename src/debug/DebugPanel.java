697d859683e288ba579a620eff87c01ab6f7a8b1
AldenSiaZhengHeng <aldensia85@gmail.com>
2021-12-02 22:25:04 +0800

    Improve the speed of the ball controller by the slider in debugpanel

--- src/debug/DebugPanel.java
+++ src/debug/DebugPanel.java
@@ -48,8 +48,8 @@ public class DebugPanel extends JPanel {
         skipLevel = makeButton("Skip Level",e -> gameControlPanel.nextLevel());
         resetBalls = makeButton("Reset Balls",e -> gameControlPanel.resetBallCount());
 
-        ballXSpeed = makeSlider(-4,4,e -> gameControlPanel.setBallXSpeed(ballXSpeed.getValue()));
-        ballYSpeed = makeSlider(-4,4,e -> gameControlPanel.setBallYSpeed(ballYSpeed.getValue()));
+        ballXSpeed = makeSlider(5,10,e -> gameControlPanel.setBallXSpeed(ballXSpeed.getValue()));
+        ballYSpeed = makeSlider(-10,-5,e -> gameControlPanel.setBallYSpeed(ballYSpeed.getValue()));
 
         this.add(skipLevel);
         this.add(resetBalls);
