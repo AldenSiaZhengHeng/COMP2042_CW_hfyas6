66f9a35f7193cc88d58875c01bb2c0db490ae820
AldenSiaZhengHeng <aldensia85@gmail.com>
2021-12-05 17:13:52 +0800

    Add javadocs in the classes in the score packages

--- src/com/BrickBreaker/score/scoreController.java
+++ src/com/BrickBreaker/score/scoreController.java
@@ -2,14 +2,31 @@ package com.BrickBreaker.score;
 
 //Import Package
 import com.BrickBreaker.element.*;
-
 import javax.swing.*;
 import java.io.*;
+
+/**
+ * This is scoreController class
+ * @author Alden Sia Zheng Heng
+ * @version 1.0
+ * @since 3/11/2021
+ */
 
 public class scoreController {
     private Brick brick;
     private scoreModel ScoreModel;
 
+    /**
+     * This is scoreController class constructor
+     */
+    public scoreController() {
+        ScoreModel = new scoreModel();
+    }
+
+    /**
+     * This class is used to give bonus score based on the ball lives
+     * @param ball_lives To compare and give bonusScore
+     */
     public void givebonus(int ball_lives){
         if(ball_lives == 3){
             ScoreModel.bonusScore(30);
@@ -24,16 +41,18 @@ public class scoreController {
         }
     }
 
+    /**
+     * This is a method to pass the penalty mark to the penalty method
+     */
     public void givePenalty(){
         ScoreModel.penalty(50);
     }
 
 
-    public scoreController() {
-        ScoreModel = new scoreModel();
-
-    }
-
+    /**
+     * To check whether the current score is higher than highscore
+     * @param highscore highest score in the list
+     */
     public void checkScore(String highscore){
         int highSc;
         String parts[] = highscore.split(":");
@@ -53,6 +72,10 @@ public class scoreController {
     }
 
 
+    /**
+     * This method is to read the txt file and return the text read
+     * @return Return the text read from the txt file
+     */
     public String GetHighScore(){
         FileReader readFile = null;
         BufferedReader reader = null;
@@ -78,6 +101,11 @@ public class scoreController {
         }
     }
 
+    /**
+     * This method is used to write the file if current score is higher than highscore
+     * @param name Player's name to write in the txt file
+     * @param score Player's score to write in the txt file
+     */
     public void writeFile(String name,int score){
         File scoreFile = new File("src/com/BrickBreaker/score/highscore.txt");
         if(!scoreFile.exists()) {
--- src/com/BrickBreaker/score/scoreModel.java
+++ src/com/BrickBreaker/score/scoreModel.java
@@ -1,27 +1,49 @@
 package com.BrickBreaker.score;
 
+/**
+ * This is scoreModel class
+ * @author Alden Sia Zheng Heng
+ * @version 1.0
+ * @since 3/11/2021
+ */
+
 public class scoreModel {
     private static int score;
 
-    public scoreModel(){
-    }
-
+    /**
+     *  This method is used to get the current player's score
+     * @return the score of the player
+     */
     public int getScore(){
         return score;
     }
 
+    /**
+     * This method is used to set the score
+     * @param Score Current player's score to be saved
+     */
     public void setScore(int Score){
         this.score = Score;
     }
 
+    /**
+     * Add the bonus score to the current score when meet the requirement
+     * @param bonus The bonus score to be added into current score
+     * @return The score that is added with bonus score
+     */
     public int bonusScore(int bonus){
         return score += bonus;
     }
 
+    /**
+     * Deduct current score when meet the penalty requirement
+     * @param loss_mark The penalty score to deduct the current score
+     * @return The score that has been deducted
+     */
     public int penalty(int loss_mark){
         if(score < loss_mark){
             score = 0;
             return 0;
         }
-        return score -= 20;}
+        return score -= loss_mark;}
 }
