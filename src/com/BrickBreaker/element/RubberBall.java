496fe3a311a90570d4af095798060ecb3a0e2279
AldenSiaZhengHeng <aldensia85@gmail.com>
2021-12-05 21:05:47 +0800

    Adjust the ball size in the game

--- src/com/BrickBreaker/element/RubberBall.java
+++ src/com/BrickBreaker/element/RubberBall.java
@@ -26,7 +26,7 @@ import java.awt.geom.Point2D;
  */
 public class RubberBall extends Ball {
 
-    private static final int DEF_RADIUS = 10;
+    private static final int DEF_RADIUS = 14;
     private static final Color DEF_INNER_COLOR = new Color(255, 219, 88);
     private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker().darker();
 
