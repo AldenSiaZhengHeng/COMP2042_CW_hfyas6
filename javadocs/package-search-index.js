c505ad735944767536c8c9024fb717efd74af15f
AldenSiaZhengHeng <aldensia85@gmail.com>
2021-12-08 22:36:21 +0800

    Push javadoc generated to the git respiratory for back-up

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.BrickBreaker"},{"l":"com.BrickBreaker.debug"},{"l":"com.BrickBreaker.element"},{"l":"com.BrickBreaker.element.ball"},{"l":"com.BrickBreaker.element.brick"},{"l":"com.BrickBreaker.gui"},{"l":"com.BrickBreaker.score"}];updateSearchResults();
